02923edad4e7d48a4d38345e2f9bfd107136275e
Michael.Zeng <michael.zengzf@gmail.com>
2019-02-02 21:29:07 -0800

    add undo

--- SuperSimpleInstallAndRun.sh
+++ SuperSimpleInstallAndRun.sh
@@ -1,8 +1,3 @@
-sudo apt update
-sudo apt install pkg-config gtk+2.0 cmake libasound2
-
-sudo wget -O /usr/local/bin/rmate https://raw.github.com/aurora/rmate/master/rmate
-sudo chmod a+x /usr/local/bin/rmate
 
 rm -rf build
 mkdir build
